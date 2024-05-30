ea354d65eb3749267f9f3202ec08a9e67a7572fa
santidotpy <santidotpy@gmail.com>
2024-05-30 04:54:49 -0300

    update TP3

--- TP3/water_heater_v3.py
+++ TP3/water_heater_v3.py
@@ -1,80 +1,18 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import math
 
-class Calentador:
-    def __init__(self, temperatura_inicial, masa, tiempo, voltaje):
-        self.temperatura = temperatura_inicial
-        self.masa = masa
-        self.set_tiempo(tiempo)
-        self.constante_calor = 4.18  # constante de calor específico del agua
-        self.voltaje = voltaje
 
-    def get_temperatura(self):
-        return self.temperatura
+cct = 0.032  # Coeficiente de Conductividad Térmica en W/mK
+espesor = 0.001  # Espesor en metros (1 mm)
+diametro = 0.1 # en metros
+altura = 0.15 # en metros
+
+def get_superficie_total():
+    # superficie total en m²
+    return 2 * math.pi * (diametro / 2) * (altura + diametro / 2)
     
-    def set_temperatura(self, temperatura):
-        self.temperatura = temperatura
-    
-    def get_masa(self):
-        return self.masa
-    
-    def set_masa(self, masa):
-        self.masa = masa
+def perdida():
+    perdida=cct*get_superficie_total()/espesor
+    return perdida
 
-    def get_tiempo(self):
-        return self.tiempo
-    
-    def set_tiempo(self, tiempo):
-        if tiempo >= 0:
-            self.tiempo = tiempo
-        else:
-            print("El tiempo debe ser positivo y en segundos")
-    
-    def get_voltaje(self):
-        return self.voltaje
-    
-    def set_voltaje(self, voltaje):
-        self.voltaje = voltaje
-
-    def calculo_delta_temp(self):
-        delta_t = 100 - self.temperatura
-        calor = self.constante_calor * self.masa * delta_t
-        potencia = calor / self.tiempo
-        # corriente = potencia / self.voltaje
-
-        # Resistencia
-        # resistencia = self.voltaje / corriente
-
-        nuevo_delta = potencia / ((self.masa / 1000) * self.constante_calor * 1000)
-        return nuevo_delta
-    
-    def graficar(self, nuevo_delta):
-        tiempo = self.tiempo
-        y = [nuevo_delta * i for i in range(1, tiempo + 1)]
-        x = np.arange(1, tiempo + 1)
-        slope, intercept = np.polyfit(x, y, 1)
-        y_line = slope * x + intercept
-
-        # muestro el gráfico
-        plt.plot(x, y_line, 'b-')
-        plt.xlabel('Tiempo (s)')
-        plt.ylabel('Delta de Temperatura')
-        plt.title('Variación de Temperatura con el Tiempo')
-        plt.grid(True)
-        plt.show()
-
-    def calcular_perdida_calor(self, coeficiente_conductividad, superficie_total, espesor_pared):
-        perdida_calor = coeficiente_conductividad * superficie_total * self.temperatura / espesor_pared
-        return perdida_calor
-
-
-calentador1 = Calentador(30, 1000, 300, 220)
-
-# Especificaciones del diseño del dispositivo
-coeficiente_conductividad = 2.1  # en Watts/metro Kelvin
-superficie_total = 1  # en metros cuadrados
-espesor_pared = 0.001  # en metros
-
-# Calcular la pérdida de calor
-perdida_calor = calentador1.calcular_perdida_calor(coeficiente_conductividad, superficie_total, espesor_pared)
-print("Pérdida de calor del dispositivo:", perdida_calor, "Watts/grado Kelvin")
+if __name__ == "__main__":
+    print(f'Perdida de calor: {perdida()} W/°K')
